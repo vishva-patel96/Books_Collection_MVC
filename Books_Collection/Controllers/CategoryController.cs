9d45a47114fac612e40010fd44f3b5b2db8e3ff5
Golu Patel <1996vishva@gmail.com>
2023-10-14 16:36:21 -0400

    Added new button for edit and delete.

--- Books_Collection/Controllers/CategoryController.cs
+++ Books_Collection/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Books_Collection.Data;
 using Books_Collection.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Books_Collection.Controllers
 {
@@ -10,13 +11,14 @@ namespace Books_Collection.Controllers
         //get data from Categories table
         public CategoryController(ApplicationContext db)
         {
-                _db = db;
+            _db = db;
         }
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        //HttpGet method
         public IActionResult Create()
         {
             return View();
@@ -25,12 +27,50 @@ namespace Books_Collection.Controllers
         public IActionResult Create(Category category)
         {
             //Custom validation
-            if(category.DisplayOrder.ToString() == category.Name)
+            if (category.DisplayOrder.ToString() == category.Name)
+            {
+                ModelState.AddModelError("name", "DisplayOrder cannot exactly match the Name.");
+            }
+            Category? CategoryfromDB = _db.Categories.FirstOrDefault(c => c.Name == category.Name);
+            if(CategoryfromDB.Name == category.Name)
+            {
+                ModelState.AddModelError("Name", "Name is already exist");
+            }
+
+            //server-side validation
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            //Custom validation
+            if (category.DisplayOrder.ToString() == category.Name)
             {
                 ModelState.AddModelError("name", "DisplayOrder cannot exactly match the Name.");
             }
             //server-side validation
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
